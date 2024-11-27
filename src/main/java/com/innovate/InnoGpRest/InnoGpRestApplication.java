f09ae4fc723bbd9a8f2886463c0abdf5b8bc4063
juanmacasgim <juanmariacastillogimenez@gmail.com>
2024-11-27 18:32:20 +0100

    First Commit

---
+++ src/main/java/com/innovate/InnoGpRest/InnoGpRestApplication.java
@@ -0,0 +1,13 @@
+package com.innovate.InnoGpRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoGpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoGpRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovate/InnoGpRest/InnoGpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovate.InnoGpRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoGpRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
